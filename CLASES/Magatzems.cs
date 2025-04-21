b14cb211b2a7413f2de823dc9faf61ddefa8ef4f
TuBroEldembo <fernando.martinez.lsgi@gmail.com>
2025-04-21 18:50:29 +0200

    CLASE MAGATZEM Y ZONES ADD

---
+++ CLASES/Magatzems.cs
@@ -0,0 +1,44 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SYNKROAPP.CLASES
+{
+    [FirestoreData]
+    public class Magatzems
+    {
+        [FirestoreProperty]
+        public int IdMagatzem { get; set; }
+
+        [FirestoreProperty]
+        public int EmpresaPertanyent { get; set; }
+
+        [FirestoreProperty]
+        public string NomMagatzem { get; set; }
+
+        [FirestoreProperty]
+        public Adreça Ubicacio { get; set; }
+
+        [FirestoreProperty]
+        public List<int> Zones { get; set; } = new List<int>();
+
+        [FirestoreProperty]
+        public bool MagatzemPerDefecte { get; set; }
+
+        public Magatzems() { }
+
+        public Magatzems(int idMagatzem, int empresaPertanyent, string nomMagatzem, Adreça ubicacio, List<int> zones, bool magatzemPerDefecte)
+        {
+            IdMagatzem = idMagatzem;
+            EmpresaPertanyent = empresaPertanyent;
+            NomMagatzem = nomMagatzem;
+            Ubicacio = ubicacio;
+            Zones = zones;
+            MagatzemPerDefecte = magatzemPerDefecte;
+        }
+    }
+
+}
---
+++ CLASES/ZonesEmmagatzematge.cs
@@ -0,0 +1,35 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SYNKROAPP.CLASES
+{
+    [FirestoreData]
+    public class ZonaEmmagatzematge
+    {
+        [FirestoreProperty]
+        public int ZonaEmmagatzematgeID { get; set; }  // idZonaEmmagatzematge
+
+        [FirestoreProperty]
+        public string Nom { get; set; }  // nom
+
+        [FirestoreProperty]
+        public int MagatzemPertanyent { get; set; }  // idMagatzem (relación con Magatzem)
+
+        [FirestoreProperty]
+        public List<int> Productes { get; set; } = new List<int>();  // Relación con ProducteInventari (lista de IDs)
+
+        public ZonaEmmagatzematge() { }
+
+        public ZonaEmmagatzematge(int idZonaEmmagatzematge, string nom, int magatzemPertanyent, List<int> productes)
+        {
+            ZonaEmmagatzematgeID = idZonaEmmagatzematge;
+            Nom = nom;
+            MagatzemPertanyent = magatzemPertanyent;
+            Productes = productes;
+        }
+    }
+}
